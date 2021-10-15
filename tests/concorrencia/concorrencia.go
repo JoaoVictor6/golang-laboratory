915379730b26f29932d8e319c77f1cb4f492c24f
Joao Victor dos Santos <santosjoao.dev@gmail.com>
2021-10-15 19:57:01 -0300

    example

---
+++ tests/concorrencia/concorrencia.go
@@ -0,0 +1,26 @@
+package concorrencia
+
+type VerificadorWebsite func(string) bool
+
+type Resultado struct {
+	string
+	bool
+}
+
+func VerificaWebsites(vw VerificadorWebsite, urls []string) map[string]bool {
+	resultados := make(map[string]bool)
+	canalResultado := make(chan Resultado)
+
+	for _, url := range urls {
+		go func(u string) {
+			canalResultado <- Resultado{string: u, bool: vw(u)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		resultado := <-canalResultado
+		resultados[resultado.string] = resultado.bool
+	}
+
+	return resultados
+}
